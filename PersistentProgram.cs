3f789ef9b19623f6293abe27aca6b003c2da1e90
vertecx <pjr+git@petr.se>
2016-10-20 13:01:28 +0200

    Separated Main functions for Quick and Persistent notifications

---
+++ PersistentProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Petr.Notify
+{
+	internal static class PersistentProgram
+	{
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+		}
+	}
+}
